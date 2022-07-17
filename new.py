e8cb3631ad325182e961eda51e3b56bdf2a6a1de
SuryanshBVerma <abcnkv4suryansh@gmail.com>
2022-07-17 13:24:18 +0530

    input file

--- new.py
+++ new.py
@@ -1,19 +1,54 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-# Draws the graphs with their edges and weights along side
-def nor_graph(n,G):
+G = nx.Graph() # Creating a global graph opbject G  
+graph_input()
+nor_graph()
+show_spath()
 
-  for i in range(1, n + 1):
+def graph_input():
+  n = int(input("Enter number of nodes : ")) # Number of Nodes
+
+  for i in range(1, n + 1): # Adding nodes
     G.add_node(i)
 
-  e = int(input("Enter number of edges : "))
-  print("Format : From_node, To_node, weight")
+  e = int(input("Enter number of edges : ")) # Number of edges
+  
+  # Check for edge
+  if not ((e > 0) and (e <= ((n*(n+1))/2-n))) :
+    print("Invalid Edge")
+    nor_graph(n,G) 
 
-  for j in range(e):
+  elif (e == 0):
+
+    plt.figure() 
+    pos = nx.shell_layout(G)
+    nx.draw(G,pos, node_color=['grey'], with_labels=True,font_color = 'white')
+    sys.exit("No shortest path possible") 
+
+  # Taking input of edges with their weights
+  print("Format : From_node, To_node, weight") 
+  
+  for j in range(e): 
     x = input()
     x = x.split(',')
-    G.add_edge(int(x[0]), int(x[1]),weight = int(x[2]))
+
+    # Check for valid node and weight 
+    if ((int(x[0]) > 0) and (int(x[0]) <= n) and (int(x[1]) > 0) and (int(x[1]) <= n) and (int(x[2]) >= 0)) :
+      G.add_edge(int(x[0]), int(x[1]),weight = int(x[2]))
+    else :
+      print("Invalid Entry")
+      nor_graph(n,G)
+
+    # Taking the sorce - destination pair and printing the graph with shortest path
+    m = int(input("Enter number of shortest path you want : "))
+    for k in range(m):
+      a = int(input("Enter source node : "))
+      b = int(input("Enter destination node : "))
+      show_spath(a,b)
+
+# nor_graph() : Plots the graph after input
+def nor_graph(): 
 
   plt.figure() 
   pos = nx.shell_layout(G)
@@ -22,15 +57,21 @@ def nor_graph(n,G):
   nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
 
 # show_spath() finds the shortest path between 2 nodes
-
 def show_spath(from_node, to_node):
+
+    # Check for valid path
     plt.figure()    
     pos = nx.shell_layout(G)
     
     weight_labels = nx.get_edge_attributes(G,'weight')
     
-    path = nx.dijkstra_path(G, source = from_node, target = to_node)
-
+    # Try - catch block for no route possible
+    try:
+     path = nx.dijkstra_path(G, source = from_node, target = to_node)
+    except Exception as err:
+      print(err)
+      return
+  
     edges_path = list(zip(path,path[1:]))
     edges_path_reversed = [(y,x) for (x,y) in edges_path]
     edges_path = edges_path + edges_path_reversed
@@ -38,9 +79,3 @@ def show_spath(from_node, to_node):
 
     nx.draw(G, pos, with_labels = True, font_color = 'white', edge_color= edge_colors,node_color=['grey']) 
     nx.draw_networkx_edge_labels(G,pos,edge_labels= weight_labels)
-
-
-G = nx.Graph()
-n = int(input("Enter number of nodes : "))
-nor_graph(n, G)
-show_spath(1,2)
